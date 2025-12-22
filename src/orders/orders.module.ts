eab1e7e51dbaeaf45a17d23daca9219996031278
eros-codes <saghafiarvin@gmail.com>
2025-12-22 17:46:51 +0330

    update3

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -6,12 +6,13 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { QrModule } from '../qr/qr.module';
 import { TableSessionGuard } from '../qr/table-session.guard';
 import { RedisModule } from '../redis/redis.module';
+import { RedisService } from '../redis/redis.service';
 import { OrdersGateway } from './orders.gateway';
 
 @Module({
   imports: [PrismaModule, QrModule, RedisModule],
   controllers: [OrdersController],
-  providers: [OrdersService, OrdersRepository, TableSessionGuard, OrdersGateway],
+  providers: [OrdersService, OrdersRepository, TableSessionGuard, OrdersGateway, RedisService],
   exports: [OrdersService, OrdersGateway],
 })
 export class OrdersModule {}
